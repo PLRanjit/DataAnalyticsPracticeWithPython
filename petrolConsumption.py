b8d42afe7cbe3a4971bc3c696c73fd1afb88b465
Ranjit <known1995ranjit@yahoo.com>
2018-09-05 17:12:34 +0530

    Update petrolConsumption.py

--- petrolConsumption.py
+++ petrolConsumption.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+
 dataset = pd.read_csv('Data/petrol_consumption.csv')
 
 X = dataset.iloc[:, 0:4].values
@@ -7,17 +12,15 @@ y = dataset.iloc[:, 4].values
 
 #print(X)
 #print(y)
-from sklearn.model_selection import train_test_split
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-from sklearn.preprocessing import StandardScaler
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-from sklearn.ensemble import RandomForestRegressor
+
 
 regressor = RandomForestRegressor(n_estimators=20, random_state=0)
 regressor.fit(X_train, y_train)
@@ -29,8 +32,6 @@ y_pred = regressor.predict(X_test)
 
 df=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})
 print(df.describe())
-
-from sklearn import metrics
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
